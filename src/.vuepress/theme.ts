7912899869179fb18277956ffca907e0c81b984d
rocky.peng <rocky.peng@qq.com>
2023-01-02 21:28:58 +0800

    删除demo posts文件夹

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -71,7 +71,7 @@ export default hopeTheme({
       // sidebar
       sidebar: zhSidebar,
 
-      footer: "<a href=\"https://beian.miit.gov.cn/\">渝ICP备2022014544号-1</a><!--&nbsp;|&nbsp; <a href=\"http://leyongjituan.com\">乐用集团</a>-->",
+      footer: "<a href=\"https://beian.miit.gov.cn/\" target=\"_blank\">渝ICP备2022014544号-1</a><!--&nbsp;|&nbsp; <a href=\"http://leyongjituan.com\">乐用集团</a>-->",
 
       displayFooter: true,
 
