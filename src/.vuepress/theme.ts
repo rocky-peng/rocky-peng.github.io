a3e400ecf01e2441573bc861c1a190611231018d
rocky.peng <rocky.peng@qq.com>
2023-01-11 21:46:05 +0800

    nothing

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -71,7 +71,13 @@ export default hopeTheme({
       // sidebar
       sidebar: zhSidebar,
 
-      footer: "<a href=\"/sitemap.xml\" target=\"_blank\">站点地图</a>&nbsp;|&nbsp;<a href=\"https://beian.miit.gov.cn/\" target=\"_blank\">渝ICP备2022014544号-1</a><!--&nbsp;|&nbsp; <a href=\"http://leyongjituan.com\">乐用集团</a>-->",
+      footer: "" +
+          "<a href=\"http://www.justsoso.fun\" target=\"_blank\">Just斑斑</a>&nbsp;|&nbsp;" +
+          "<a href=\"http://docs.justdopay.com\" target=\"_blank\">Rocky的博客</a>&nbsp;|&nbsp;" +
+          "<a href=\"http://zy.justdopay.com\" target=\"_blank\">资源之家</a>&nbsp;|&nbsp;" +
+          "<a href=\"/sitemap.xml\" target=\"_blank\">站点地图</a>&nbsp;|&nbsp;" +
+          "<a href=\"https://beian.miit.gov.cn/\" target=\"_blank\">渝ICP备2022014544号-1</a>" +
+          "<!--&nbsp;|&nbsp; <a href=\"http://leyongjituan.com\">乐用集团</a>-->",
 
       displayFooter: true,
 
