90d0296ca2548820839756343dfbdbb40c7c7adb
rocky-peng <rocky.peng@qq.com>
2024-09-08 13:20:14 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@f3a67bf9788a721e0ecf1c5e73c4ffe9f3c975b5 🚀

---
+++ assets/Cordova_Umi项目搭建步骤.html-9a04d855.js
@@ -0,0 +1,81 @@
+import{_ as v,$ as o,a0 as c,a1 as e,a2 as i,a3 as n,a4 as d,a5 as r,w as s}from"./framework-58787b1c.js";const t={},u=r(`<h2 id="项目搭建" tabindex="-1"><a class="header-anchor" href="#项目搭建" aria-hidden="true">#</a> 项目搭建</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 创建 cordova 项目： cordova-demo
+cordova create cordova-demo com.example.hello CordovaDemo
+# 上面的命令会自动创建 cordova-demo 目录
+
+
+
+# 开始创建umi项目
+cd cordova-demo
+mkdir myapp-a &amp;&amp; cd myapp-a
+yarn create umi
+
+# 选择app
+# 不启用typescript
+# 勾选antd dva
+# 回车
+
+# 执行完后，执行
+
+yarn or npm install
+
+# 修改 .umirc.js 在 export default { 中添加下面三行代码
+  outputPath: &#39;../www/&#39;,
+  publicPath: &#39;./&#39;,
+  history: &#39;hash&#39;,
+
+# 然后执行
+npm run build
+
+# 上面执行完后，会将 cordova-demo 目录中原有的www目录给删掉，然后重新创建www目录，存放的是umi项目的打包后的文件
+
+# 这个时候返回cordova-demo目录
+cd ..
+
+# 测试 cordova android
+
+cordova platform add android
+cordova build android
+
+# 添加 .gitignore 文件
+
+在 cordova-demo 创建 .gitignore 文件，内容如下：
+
+.idea
+node_modules
+plugins
+platforms
+www
+
+在 myapp-a 创建 .gitignore 文件，内容如下：
+
+
+# dependencies
+/node_modules
+/npm-debug.log*
+/yarn-error.log
+/yarn.lock
+/package-lock.json
+
+# production
+/dist
+
+# misc
+.DS_Store
+
+# umi
+.umi
+.umi-production
+
+结束
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="扩展资料" tabindex="-1"><a class="header-anchor" href="#扩展资料" aria-hidden="true">#</a> 扩展资料</h2>`,3),m={href:"https://cordova.apache.org/docs/en/latest/guide/cli/index.html",target:"_blank",rel:"noopener noreferrer"},b={href:"https://umijs.org/zh/guide/create-umi-app.html#%E5%88%9B%E5%BB%BA-umi-%E9%A1%B9%E7%9B%AE",target:"_blank",rel:"noopener noreferrer"},E=r(`<h2 id="cordova-打包" tabindex="-1"><a class="header-anchor" href="#cordova-打包" aria-hidden="true">#</a> cordova 打包</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>debug包
+
+cordova build android -- --gradleArg=-PcdvVersionCode=2
+
+
+release包
+cordova build android --release -- --gradleArg=-PcdvVersionCode=2
+
+传递参数给gradle
+-- --gradleArg=-PcdvVersionCode=2 --gradleArg=-PVERSION_NAME=2.2.2
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,4),h=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),p=e("hr",null,null,-1),B=e("hr",null,null,-1),_=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),i("：解释这个东西，让我看起来像个罪人。 "),e("br"),e("br"),e("img",{src:"https://tuapi.eees.cc/api.php?category=biying&type=302&uuid=a195c18b-9b07-4d74-b225-589233d15ba2",alt:"",loading:"lazy"})])],-1);function A(g,f){const a=s("ExternalLinkIcon"),l=s("RouterLink");return o(),c("div",null,[u,e("p",null,[i("cordova : "),e("a",m,[i("https://cordova.apache.org/docs/en/latest/guide/cli/index.html"),n(a)])]),e("p",null,[i("umi: "),e("a",b,[i("https://umijs.org/zh/guide/create-umi-app.html#创建-umi-项目"),n(a)])]),E,e("ul",null,[e("li",null,[n(l,{to:"/other/Git%E5%90%88%E5%B9%B6%E5%A4%9A%E4%B8%AA%E6%8F%90%E4%BA%A4%E5%B9%B6push%E5%88%B0%E8%BF%9C%E7%A8%8B%E4%BB%93%E5%BA%93.html"},{default:d(()=>[i("Git合并多个提交并push到远程仓库")]),_:1})]),e("li",null,[n(l,{to:"/other/Nacos-Spring%20Gateway-Spring%20boot%E6%97%A0%E6%84%9F%E5%8F%91%E5%B8%83.html"},{default:d(()=>[i("Nacos-Spring Gateway-Spring boot无感发布")]),_:1})]),e("li",null,[n(l,{to:"/other/PostgreSQL%E5%AE%9A%E6%97%B6%E5%A4%87%E4%BB%BD.html"},{default:d(()=>[i("PostgreSQL定时备份")]),_:1})]),e("li",null,[n(l,{to:"/software/middleware/mysql/%E5%BC%82%E5%9C%B0%E5%A4%9A%E6%B4%BB.html"},{default:d(()=>[i("异地多活")]),_:1})]),e("li",null,[n(l,{to:"/software/middleware/mysql/MySQL%E7%B4%A2%E5%BC%95.html"},{default:d(()=>[i("MySQL索引")]),_:1})]),e("li",null,[n(l,{to:"/software/java-basic/AQS%E6%BA%90%E7%A0%81%E8%A7%A3%E8%AF%BB.html"},{default:d(()=>[i("AQS源码解读")]),_:1})]),e("li",null,[n(l,{to:"/software/middleware/mysql/SQL%E4%BC%98%E5%8C%96.html"},{default:d(()=>[i("SQL优化")]),_:1})]),e("li",null,[n(l,{to:"/software/java-basic/Java%E9%9D%A2%E8%AF%95%E5%9F%BA%E7%A1%80.html"},{default:d(()=>[i("Java面试基础")]),_:1})]),e("li",null,[n(l,{to:"/other/Git%E5%8E%86%E5%8F%B2%E8%AE%B0%E5%BD%95%E4%BF%AE%E6%94%B9%E7%94%A8%E6%88%B7%E5%90%8D%E5%92%8C%E9%82%AE%E7%AE%B1.html"},{default:d(()=>[i("Git历史记录修改用户名和邮箱")]),_:1})]),e("li",null,[n(l,{to:"/other/PostgreSQL%20JSON%E7%B1%BB%E5%9E%8B%E5%AD%97%E6%AE%B5%E5%B8%B8%E7%94%A8%E6%93%8D%E4%BD%9C.html"},{default:d(()=>[i("PostgreSQL JSON类型字段常用操作")]),_:1})]),e("li",null,[n(l,{to:"/other/MySQL%E6%95%B0%E6%8D%AE%E8%BF%81%E7%A7%BB%E5%88%B0PGSQL.html"},{default:d(()=>[i("MySQL数据迁移到PGSQL")]),_:1})]),e("li",null,[n(l,{to:"/other/SpringBoot%E6%9C%8D%E5%8A%A1%E5%9C%A8%E6%9C%8D%E5%8A%A1%E5%90%AF%E5%8A%A8%E5%AE%8C%E6%88%90%E5%89%8D%E8%A2%AB%E6%8F%90%E5%89%8D%E6%B3%A8%E5%86%8C%E5%88%B0nacos.html"},{default:d(()=>[i("SpringBoot服务在服务启动完成前被提前注册到nacos")]),_:1})])]),h,p,B,_])}const w=v(t,[["render",A],["__file","Cordova_Umi项目搭建步骤.html.vue"]]);export{w as default};
