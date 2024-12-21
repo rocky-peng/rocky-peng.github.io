ae3cb57a21decc2d4febf5323d48dd99ab585af2
rocky-peng <rocky.peng@qq.com>
2024-12-21 07:05:38 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@28af39ea76f6f07fa1f347f2bbb3d4244b29e2ea 🚀

---
+++ assets/Cordova_Umi项目搭建步骤.html-a6f44358.js
@@ -0,0 +1,81 @@
+import{_ as o,$ as v,a0 as c,a1 as e,a2 as i,a3 as n,a4 as l,a5 as r,w as s}from"./framework-b3a0f150.js";const t={},u=r(`<h2 id="项目搭建" tabindex="-1"><a class="header-anchor" href="#项目搭建" aria-hidden="true">#</a> 项目搭建</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 创建 cordova 项目： cordova-demo
+cordova create cordova-demo com.example.hello CordovaDemo
+# 上面的命令会自动创建 cordova-demo 目录
+
+
+
+# 开始创建umi项目
+cd cordova-demo
+mkdir myapp-a &amp;&amp; cd myapp-a
+yarn create umi
+
+# 选择app
+# 不启用typescript
+# 勾选antd dva
+# 回车
+
+# 执行完后，执行
+
+yarn or npm install
+
+# 修改 .umirc.js 在 export default { 中添加下面三行代码
+  outputPath: &#39;../www/&#39;,
+  publicPath: &#39;./&#39;,
+  history: &#39;hash&#39;,
+
+# 然后执行
+npm run build
+
+# 上面执行完后，会将 cordova-demo 目录中原有的www目录给删掉，然后重新创建www目录，存放的是umi项目的打包后的文件
+
+# 这个时候返回cordova-demo目录
+cd ..
+
+# 测试 cordova android
+
+cordova platform add android
+cordova build android
+
+# 添加 .gitignore 文件
+
+在 cordova-demo 创建 .gitignore 文件，内容如下：
+
+.idea
+node_modules
+plugins
+platforms
+www
+
+在 myapp-a 创建 .gitignore 文件，内容如下：
+
+
+# dependencies
+/node_modules
+/npm-debug.log*
+/yarn-error.log
+/yarn.lock
+/package-lock.json
+
+# production
+/dist
+
+# misc
+.DS_Store
+
+# umi
+.umi
+.umi-production
+
+结束
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="扩展资料" tabindex="-1"><a class="header-anchor" href="#扩展资料" aria-hidden="true">#</a> 扩展资料</h2>`,3),m={href:"https://cordova.apache.org/docs/en/latest/guide/cli/index.html",target:"_blank",rel:"noopener noreferrer"},b={href:"https://umijs.org/zh/guide/create-umi-app.html#%E5%88%9B%E5%BB%BA-umi-%E9%A1%B9%E7%9B%AE",target:"_blank",rel:"noopener noreferrer"},h=r(`<h2 id="cordova-打包" tabindex="-1"><a class="header-anchor" href="#cordova-打包" aria-hidden="true">#</a> cordova 打包</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>debug包
+
+cordova build android -- --gradleArg=-PcdvVersionCode=2
+
+
+release包
+cordova build android --release -- --gradleArg=-PcdvVersionCode=2
+
+传递参数给gradle
+-- --gradleArg=-PcdvVersionCode=2 --gradleArg=-PVERSION_NAME=2.2.2
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,4),E=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),p=e("hr",null,null,-1),_=e("hr",null,null,-1),B=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),i("：困难就像弹簧，你弱他就强，你强它更强。 "),e("br"),e("br"),e("img",{src:"https://tuapi.eees.cc/api.php?category=biying&type=302&uuid=940ea35d-c4ee-4650-ac2e-58c8d2903b71",alt:"",loading:"lazy"})])],-1);function g(f,A){const a=s("ExternalLinkIcon"),d=s("RouterLink");return v(),c("div",null,[u,e("p",null,[i("cordova : "),e("a",m,[i("https://cordova.apache.org/docs/en/latest/guide/cli/index.html"),n(a)])]),e("p",null,[i("umi: "),e("a",b,[i("https://umijs.org/zh/guide/create-umi-app.html#创建-umi-项目"),n(a)])]),h,e("ul",null,[e("li",null,[n(d,{to:"/other/Notion%E7%AC%94%E8%AE%B0%E5%AE%9A%E6%97%B6%E5%A4%87%E4%BB%BD.html"},{default:l(()=>[i("Notion笔记定时备份")]),_:1})]),e("li",null,[n(d,{to:"/software/unclassified/KVM%20%E8%99%9A%E6%8B%9F%E6%9C%BA%E5%AE%89%E8%A3%85.html"},{default:l(()=>[i("KVM 虚拟机安装")]),_:1})]),e("li",null,[n(d,{to:"/software/middleware/mysql/MySQL%E5%B8%B8%E7%94%A8%E5%91%BD%E4%BB%A4.html"},{default:l(()=>[i("MySQL常用命令")]),_:1})]),e("li",null,[n(d,{to:"/other/Hadoop%20%E4%B8%80.html"},{default:l(()=>[i("Hadoop 一")]),_:1})]),e("li",null,[n(d,{to:"/software/unclassified/%E6%89%B9%E9%87%8F%E4%BF%AE%E6%94%B9git%E5%8E%86%E5%8F%B2%E8%AE%B0%E5%BD%95%E4%B8%AD%E7%9A%84%E7%94%A8%E6%88%B7%E5%90%8D%E5%92%8C%E9%82%AE%E7%AE%B1.html"},{default:l(()=>[i("批量修改git历史记录中的用户名和邮箱")]),_:1})]),e("li",null,[n(d,{to:"/other/Paxos%E7%AE%97%E6%B3%95.html"},{default:l(()=>[i("Paxos算法")]),_:1})]),e("li",null,[n(d,{to:"/software/unclassified/%E5%88%B6%E4%BD%9CKVM%20ES%E9%95%9C%E5%83%8F%E6%96%87%E4%BB%B6.html"},{default:l(()=>[i("制作KVM ES镜像文件")]),_:1})]),e("li",null,[n(d,{to:"/software/middleware/mysql/%E5%BC%82%E5%9C%B0%E5%A4%9A%E6%B4%BB.html"},{default:l(()=>[i("异地多活")]),_:1})]),e("li",null,[n(d,{to:"/other/PGSQL%E7%9A%84json%E5%92%8C%20jsonb%20%E8%AF%BB%E5%86%99%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95.html"},{default:l(()=>[i("PGSQL的json和 jsonb 读写性能测试")]),_:1})]),e("li",null,[n(d,{to:"/software/java-basic/CountDownLatch%E6%BA%90%E7%A0%81%E8%A7%A3%E8%AF%BB.html"},{default:l(()=>[i("CountDownLatch源码解读")]),_:1})]),e("li",null,[n(d,{to:"/software/middleware/mq/RocketMQ.html"},{default:l(()=>[i("RocketMQ")]),_:1})]),e("li",null,[n(d,{to:"/other/GitHub%20Workflow%E7%AA%81%E7%84%B6%E6%8A%A5%E9%94%99.html"},{default:l(()=>[i("GitHub Workflow突然报错")]),_:1})])]),E,p,_,B])}const x=o(t,[["render",g],["__file","Cordova_Umi项目搭建步骤.html.vue"]]);export{x as default};
