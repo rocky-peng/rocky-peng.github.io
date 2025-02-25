00293666aafe89f298452fa9c364d17fd00d743c
rocky-peng <rocky.peng@qq.com>
2025-02-25 07:12:57 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@b14342e1095286b44e45325b17e95863c76ab56c 🚀

---
+++ assets/Cordova_Umi项目搭建步骤.html-9901cf1c.js
@@ -0,0 +1,81 @@
+import{_ as v,$ as o,a0 as c,a1 as e,a2 as i,a3 as n,a4 as l,a5 as r,w as s}from"./framework-b3a0f150.js";const u={},t=r(`<h2 id="项目搭建" tabindex="-1"><a class="header-anchor" href="#项目搭建" aria-hidden="true">#</a> 项目搭建</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 创建 cordova 项目： cordova-demo
+cordova create cordova-demo com.example.hello CordovaDemo
+# 上面的命令会自动创建 cordova-demo 目录
+
+
+
+# 开始创建umi项目
+cd cordova-demo
+mkdir myapp-a &amp;&amp; cd myapp-a
+yarn create umi
+
+# 选择app
+# 不启用typescript
+# 勾选antd dva
+# 回车
+
+# 执行完后，执行
+
+yarn or npm install
+
+# 修改 .umirc.js 在 export default { 中添加下面三行代码
+  outputPath: &#39;../www/&#39;,
+  publicPath: &#39;./&#39;,
+  history: &#39;hash&#39;,
+
+# 然后执行
+npm run build
+
+# 上面执行完后，会将 cordova-demo 目录中原有的www目录给删掉，然后重新创建www目录，存放的是umi项目的打包后的文件
+
+# 这个时候返回cordova-demo目录
+cd ..
+
+# 测试 cordova android
+
+cordova platform add android
+cordova build android
+
+# 添加 .gitignore 文件
+
+在 cordova-demo 创建 .gitignore 文件，内容如下：
+
+.idea
+node_modules
+plugins
+platforms
+www
+
+在 myapp-a 创建 .gitignore 文件，内容如下：
+
+
+# dependencies
+/node_modules
+/npm-debug.log*
+/yarn-error.log
+/yarn.lock
+/package-lock.json
+
+# production
+/dist
+
+# misc
+.DS_Store
+
+# umi
+.umi
+.umi-production
+
+结束
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="扩展资料" tabindex="-1"><a class="header-anchor" href="#扩展资料" aria-hidden="true">#</a> 扩展资料</h2>`,3),m={href:"https://cordova.apache.org/docs/en/latest/guide/cli/index.html",target:"_blank",rel:"noopener noreferrer"},b={href:"https://umijs.org/zh/guide/create-umi-app.html#%E5%88%9B%E5%BB%BA-umi-%E9%A1%B9%E7%9B%AE",target:"_blank",rel:"noopener noreferrer"},h=r(`<h2 id="cordova-打包" tabindex="-1"><a class="header-anchor" href="#cordova-打包" aria-hidden="true">#</a> cordova 打包</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>debug包
+
+cordova build android -- --gradleArg=-PcdvVersionCode=2
+
+
+release包
+cordova build android --release -- --gradleArg=-PcdvVersionCode=2
+
+传递参数给gradle
+-- --gradleArg=-PcdvVersionCode=2 --gradleArg=-PVERSION_NAME=2.2.2
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,4),E=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),B=e("hr",null,null,-1),p=e("hr",null,null,-1),_=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),i("：撞衫的绝大部分原因，是便宜。 "),e("br"),e("br"),e("img",{src:"https://tuapi.eees.cc/api.php?category=biying&type=302&uuid=26807083-72d9-4a05-8be1-a19ef1d44392",alt:"",loading:"lazy"})])],-1);function g(A,f){const a=s("ExternalLinkIcon"),d=s("RouterLink");return o(),c("div",null,[t,e("p",null,[i("cordova : "),e("a",m,[i("https://cordova.apache.org/docs/en/latest/guide/cli/index.html"),n(a)])]),e("p",null,[i("umi: "),e("a",b,[i("https://umijs.org/zh/guide/create-umi-app.html#创建-umi-项目"),n(a)])]),h,e("ul",null,[e("li",null,[n(d,{to:"/software/docker/Docker%E8%BF%9B%E8%A1%8C%E8%B5%84%E6%BA%90%E9%9A%94%E7%A6%BB.html"},{default:l(()=>[i("Docker进行资源隔离")]),_:1})]),e("li",null,[n(d,{to:"/other/CloudFlare%20%E5%AE%A2%E6%88%B7%E7%AB%AF%E8%AF%81%E4%B9%A6%E7%9A%84%E4%BD%BF%E7%94%A8.html"},{default:l(()=>[i("CloudFlare 客户端证书的使用")]),_:1})]),e("li",null,[n(d,{to:"/software/unclassified/Markdown%20Mermaid%E7%94%BB%E5%9B%BE%E5%AE%9E%E4%BE%8B.html"},{default:l(()=>[i("Markdown Mermaid画图实例")]),_:1})]),e("li",null,[n(d,{to:"/other/Sublime%20Text%20%E6%A0%BC%E5%BC%8F%E5%8C%96JSON.html"},{default:l(()=>[i("Sublime Text 格式化JSON")]),_:1})]),e("li",null,[n(d,{to:"/software/java-basic/IO%E7%9B%B8%E5%85%B3.html"},{default:l(()=>[i("IO相关")]),_:1})]),e("li",null,[n(d,{to:"/software/docker/Centos%E7%A6%BB%E7%BA%BF%E5%AE%89%E8%A3%85Docker.html"},{default:l(()=>[i("Centos离线安装Docker")]),_:1})]),e("li",null,[n(d,{to:"/software/unclassified/%E5%88%86%E5%B8%83%E5%BC%8F%E9%97%AE%E9%A2%98.html"},{default:l(()=>[i("分布式问题")]),_:1})]),e("li",null,[n(d,{to:"/software/docker/Docker%E8%B7%A8%E4%B8%BB%E6%9C%BA%E9%80%9A%E4%BF%A1%E6%96%B9%E6%A1%88.html"},{default:l(()=>[i("Docker跨主机通信方案")]),_:1})]),e("li",null,[n(d,{to:"/other/MySQL%E6%95%B0%E6%8D%AE%E8%BF%81%E7%A7%BB%E5%88%B0PGSQL.html"},{default:l(()=>[i("MySQL数据迁移到PGSQL")]),_:1})]),e("li",null,[n(d,{to:"/other/%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87%E5%9C%A8%E7%BA%BF%E6%8E%A5%E5%8F%A3.html"},{default:l(()=>[i("随机图片在线接口")]),_:1})]),e("li",null,[n(d,{to:"/other/K8S%E5%B8%B8%E7%94%A8%E5%91%BD%E4%BB%A4.html"},{default:l(()=>[i("K8S常用命令")]),_:1})]),e("li",null,[n(d,{to:"/other/Linux%20dev%20shm%E7%9B%AE%E5%BD%95.html"},{default:l(()=>[i("Linux dev shm目录")]),_:1})])]),E,B,p,_])}const k=v(u,[["render",g],["__file","Cordova_Umi项目搭建步骤.html.vue"]]);export{k as default};
---
+++ assets/前后端常用技术.html-09d8ed14.js
@@ -0,0 +1,77 @@
+import{_ as c,$ as d,a0 as o,a1 as n,a2 as s,a3 as a,a4 as i,a5 as l,w as r}from"./framework-b3a0f150.js";const p={},u=l('<h2 id="后端常用技术" tabindex="-1"><a class="header-anchor" href="#后端常用技术" aria-hidden="true">#</a> 后端常用技术</h2><ol><li><p>支付 支付宝、微信</p></li><li><p>日志收集</p><ul><li><p>日志json化</p></li><li><p>ELK</p></li></ul></li><li><p>环境配置</p></li><li><p>代码发布平台jenkins</p></li></ol><h2 id="如何快速迭代app" tabindex="-1"><a class="header-anchor" href="#如何快速迭代app" aria-hidden="true">#</a> 如何快速迭代app：</h2><ol><li>native + flutter</li><li></li></ol><h2 id="app常见功能" tabindex="-1"><a class="header-anchor" href="#app常见功能" aria-hidden="true">#</a> App常见功能：</h2><ol><li><p>IM 融云（付费）、小米（全免费）</p></li><li><p>支付 支付宝、微信</p></li><li><p>推送 个推、极光</p></li><li><p>地图 高德、腾讯、百度</p></li><li><p>各种社交账号分享、登陆</p></li><li><p>统计 友盟、小米、</p></li><li><p>拍照、录像</p></li></ol><h2 id="安卓" tabindex="-1"><a class="header-anchor" href="#安卓" aria-hidden="true">#</a> 安卓</h2>',7),v={href:"https://blog.csdn.net/linidserggbvxdsefgh/article/details/78249016",target:"_blank",rel:"noopener noreferrer"},m=n("p",null,"期望类似效果：使用命令行打包测试、正式环境的包，并能指定版本，同时打包的apk文件名带有版本号。",-1),b=l(`<li><p>使用jenkins集成打包，并上传蒲公英</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment">#shell命令：</span>
+
+<span class="token comment">#!/usr/bin/env bash</span>
+<span class="token assign-left variable">versionCode</span><span class="token operator">=</span><span class="token variable"><span class="token variable">\`</span><span class="token function">date</span> +%y<span class="token variable">\`</span></span>
+<span class="token assign-left variable">versionName</span><span class="token operator">=</span><span class="token variable"><span class="token variable">\`</span><span class="token function">date</span> +%y.%m.%d<span class="token variable">\`</span></span>
+
+<span class="token comment"># 正式包和测试包，版本19.2.14 ：  gradle clean assemble -PVERSION_CODE=19  -PVERSION_NAME=19.2.14</span>
+<span class="token comment"># 正式包， 版本19.2.13  ： gradle clean assembleRelease -PVERSION_CODE=19  -PVERSION_NAME=19.2.13</span>
+<span class="token comment"># 测试包， 版本19.2.15  ： gradle clean assembleDebug -PVERSION_CODE=19  -PVERSION_NAME=19.2.15</span>
+
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;&#39;</span>
+<span class="token assign-left variable">gradlebin</span><span class="token operator">=</span><span class="token variable"><span class="token variable">\`</span><span class="token function">which</span> gradle<span class="token variable">\`</span></span>
+<span class="token comment">#echo &#39;gradle命令：&#39;\${gradlebin}</span>
+<span class="token assign-left variable">cmd</span><span class="token operator">=</span><span class="token string">&#39;&#39;</span><span class="token variable">\${gradlebin}</span><span class="token string">&#39; clean assemble -PVERSION_CODE=&#39;</span><span class="token variable">\${versionCode}</span><span class="token string">&#39;  -PVERSION_NAME=&#39;</span><span class="token variable">\${versionName}</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;执行打包命令：&#39;</span><span class="token variable">\${cmd}</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;&#39;</span>
+<span class="token variable">\${cmd}</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;&#39;</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;&#39;</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;打包成功&#39;</span>
+<span class="token builtin class-name">echo</span> <span class="token string">&#39;&#39;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li>`,1),h={href:"https://www.pgyer.com/doc/view/jenkins",target:"_blank",rel:"noopener noreferrer"},g=l(`<h2 id="git" tabindex="-1"><a class="header-anchor" href="#git" aria-hidden="true">#</a> GIT</h2><ol><li>清空所有commit记录</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>说明：例如将代码提交到git仓库，将一些敏感信息提交，所以需要删除提交记录以彻底清除提交信息，以得到一个干净的仓库且代码不变
+
+<span class="token number">1</span>.Checkout
+<span class="token function">git</span> checkout <span class="token parameter variable">--orphan</span> latest_branch
+
+<span class="token number">2</span>. Add all the files
+<span class="token function">git</span> <span class="token function">add</span> <span class="token parameter variable">-A</span>
+
+<span class="token number">3</span>. Commit the changes
+<span class="token function">git</span> commit <span class="token parameter variable">-am</span> <span class="token string">&quot;commit message&quot;</span>
+
+<span class="token number">4</span>. Delete the branch
+<span class="token function">git</span> branch <span class="token parameter variable">-D</span> master
+
+<span class="token number">5</span>.Rename the current branch to master
+<span class="token function">git</span> branch <span class="token parameter variable">-m</span> master
+
+<span class="token number">6</span>.Finally, force update your repository
+<span class="token function">git</span> push <span class="token parameter variable">-f</span> origin master
+
+大功告成
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol start="2"><li>创建新的git仓库</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>Existing folder
+
+<span class="token builtin class-name">cd</span> existing_folder
+<span class="token function">git</span> init
+<span class="token function">git</span> remote <span class="token function">add</span> origin git@gitlab.com:leyongtech/laijiandu-api-bak.git
+<span class="token function">git</span> <span class="token function">add</span> <span class="token builtin class-name">.</span>
+<span class="token function">git</span> commit <span class="token parameter variable">-m</span> <span class="token string">&quot;Initial commit&quot;</span>
+<span class="token function">git</span> push <span class="token parameter variable">-u</span> origin master
+
+Existing Git repository
+
+<span class="token builtin class-name">cd</span> existing_repo
+<span class="token function">git</span> remote <span class="token function">rename</span> origin old-origin
+<span class="token function">git</span> remote <span class="token function">add</span> origin git@gitlab.com:leyongtech/laijiandu-api-bak.git
+<span class="token function">git</span> push <span class="token parameter variable">-u</span> origin <span class="token parameter variable">--all</span>
+<span class="token function">git</span> push <span class="token parameter variable">-u</span> origin <span class="token parameter variable">--tags</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="翻墙" tabindex="-1"><a class="header-anchor" href="#翻墙" aria-hidden="true">#</a> 翻墙</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>各种系统的shadowsocks客户端:
+
+https://shadowsocks.org/en/download/clients.html
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="ssh工具" tabindex="-1"><a class="header-anchor" href="#ssh工具" aria-hidden="true">#</a> ssh工具</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>juicessh:
+
+官网：
+https://juicessh.com/
+
+安卓：
+https://play.google.com/store/apps/details?id=com.sonelli.juicessh
+
+
+terminus:
+
+安卓：
+https://play.google.com/store/apps/details?id=com.server.auditor.ssh.client
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,11),k=n("p",null,[n("br"),n("br"),n("br"),n("br"),n("br"),n("br")],-1),E=n("hr",null,null,-1),f=n("hr",null,null,-1),_=n("ul",null,[n("li",null,[n("strong",null,"随机毒鸡汤"),s("：直接拒绝，比看到信息不回复，善良一百倍。 "),n("br"),n("br"),n("img",{src:"https://tuapi.eees.cc/api.php?category=fengjing&type=302&uuid=0920d51e-e8a9-4064-af0a-d8ffd63600a0",alt:"",loading:"lazy"})])],-1);function B(A,x){const t=r("ExternalLinkIcon"),e=r("RouterLink");return d(),o("div",null,[u,n("ol",null,[n("li",null,[n("p",null,[s("使用gradle配置正式环境、测试环境的变量 "),n("a",v,[s("https://blog.csdn.net/linidserggbvxdsefgh/article/details/78249016"),a(t)])]),m]),b,n("li",null,[n("p",null,[s("配置蒲公英，进行上传 "),n("a",h,[s("https://www.pgyer.com/doc/view/jenkins"),a(t)])])])]),g,n("ul",null,[n("li",null,[a(e,{to:"/software/unclassified/MAT%E5%B7%A5%E5%85%B7.html"},{default:i(()=>[s("MAT工具")]),_:1})]),n("li",null,[a(e,{to:"/other/%E5%BE%AE%E5%8D%9A%E5%85%B3%E6%B3%A8%E5%85%B3%E7%B3%BB%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0.html"},{default:i(()=>[s("微博关注关系如何实现")]),_:1})]),n("li",null,[a(e,{to:"/software/jvm/JDK%E5%91%BD%E4%BB%A4%E8%A1%8C%E5%B7%A5%E5%85%B7.html"},{default:i(()=>[s("JDK命令行工具")]),_:1})]),n("li",null,[a(e,{to:"/other/MongoDB%E9%AB%98%E5%8F%AF%E7%94%A8.html"},{default:i(()=>[s("MongoDB高可用")]),_:1})]),n("li",null,[a(e,{to:"/other/%E6%98%AF%E6%97%B6%E5%80%99%E4%BD%BF%E7%94%A8MapStruct%E6%9B%BF%E4%BB%A3BeanUtils%E4%BA%86.html"},{default:i(()=>[s("是时候使用MapStruct替代BeanUtils了")]),_:1})]),n("li",null,[a(e,{to:"/other/%E4%BD%A0%E7%9C%9F%E7%9A%84%E4%BC%9A%E6%8B%BC%E6%8E%A5%E5%AD%97%E7%AC%A6%E4%B8%B2%E5%90%97%EF%BC%9F.html"},{default:i(()=>[s("你真的会拼接字符串吗？")]),_:1})]),n("li",null,[a(e,{to:"/other/Spring%20Boot%E5%8D%87%E7%BA%A7%E5%88%B02%206%20x%E8%B8%A9%E7%9A%84%E5%9D%91.html"},{default:i(()=>[s("Spring Boot升级到2 6 x踩的坑")]),_:1})]),n("li",null,[a(e,{to:"/software/middleware/mysql/MySQL%E7%B4%A2%E5%BC%95.html"},{default:i(()=>[s("MySQL索引")]),_:1})]),n("li",null,[a(e,{to:"/other/MyBatis%20xml%E7%89%B9%E6%AE%8A%E5%AD%97%E7%AC%A6%E5%A4%84%E7%90%86.html"},{default:i(()=>[s("MyBatis xml特殊字符处理")]),_:1})]),n("li",null,[a(e,{to:"/software/java-basic/Java%E9%9D%A2%E8%AF%95%E5%9F%BA%E7%A1%80.html"},{default:i(()=>[s("Java面试基础")]),_:1})]),n("li",null,[a(e,{to:"/other/GitHub%20Workflow%E7%AA%81%E7%84%B6%E6%8A%A5%E9%94%99.html"},{default:i(()=>[s("GitHub Workflow突然报错")]),_:1})]),n("li",null,[a(e,{to:"/software/unclassified/BBR%E5%8A%A0%E9%80%9F.html"},{default:i(()=>[s("BBR加速")]),_:1})])]),k,E,f,_])}const D=c(p,[["render",B],["__file","前后端常用技术.html.vue"]]);export{D as default};
