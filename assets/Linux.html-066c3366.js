27dec496f5b7aab7aee16a1b66bb029e029f2d5c
rocky-peng <rocky.peng@qq.com>
2025-03-07 07:13:27 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@9e564efd2688ceb541604e238ac144fd96231777 🚀

---
+++ assets/Linux.html-066c3366.js
@@ -0,0 +1,80 @@
+import{_ as d,$ as a,a0 as t,a1 as e,a3 as l,a4 as s,a5 as r,a2 as n,w as u}from"./framework-b3a0f150.js";const v={},c=r(`<h1 id="linux安装字体" tabindex="-1"><a class="header-anchor" href="#linux安装字体" aria-hidden="true">#</a> linux安装字体</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. yum install -y fontconfig mkfontscale
+2. 拷贝字体文件（.ttc文件） 到 /usr/share/fonts/ 下的一个新文件夹(A) 中
+3. chmod -R 755 /usr/share/fonts/A
+4. cd /usr/share/fonts/A
+5. mkfontscale
+6. mkfontdir
+7. fc-cache -fv
+8. 检查是否安装成功：fc-list
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="linux测速" tabindex="-1"><a class="header-anchor" href="#linux测速" aria-hidden="true">#</a> Linux测速</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  
+
+chmod +x speedtest-cli
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="常用命令" tabindex="-1"><a class="header-anchor" href="#常用命令" aria-hidden="true">#</a> 常用命令</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>查看某个目录及子目录占用的空间
+du -h
+ly-zuche-c1@root:/var/log# du -h
+36K	    ./anaconda
+536K	./nginx
+64K	    ./redis
+4.0K	./chrony
+55M	    ./mysql
+4.0K	./ppp
+4.0K	./ntpstats
+5.5M	./httpd
+8.1M	./journal/f9d400c5e1e8c3a8209e990d887d4ac1
+8.1M	./journal
+26M	    ./audit
+181M	./leyong
+12K	    ./tuned
+72M	    ./filebeat
+619M	.
+
+查看某个目录及子目录占用的空间并排序输出
+du -sh * | sort -nr 
+ly-zuche-c1@root:/var# du -sh * | sort -nr 
+619M	log
+370M	cache
+158M	www
+112K	spool
+27G	    lib
+16K	    db  
+12K	    tmp
+12K	    kerberos
+
+查看某个目录占用的空间
+du -sh
+ly-zuche-c1@root:/var# du -sh
+28G	.
+
+查看磁盘使用情况
+df -lh 
+ly-zuche-c1@root:/var# df -lh
+文件系统        容量  已用  可用 已用% 挂载点
+/dev/vda1        50G   34G   14G   72% /
+devtmpfs        1.9G     0  1.9G    0% /dev
+tmpfs           1.9G   24K  1.9G    1% /dev/shm
+tmpfs           1.9G  460K  1.9G    1% /run
+tmpfs           1.9G     0  1.9G    0% /sys/fs/cgroup
+tmpfs           380M     0  380M    0% /run/user/0
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="大分割和小文件合并" tabindex="-1"><a class="header-anchor" href="#大分割和小文件合并" aria-hidden="true">#</a> 大分割和小文件合并</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>大文件分割为小文件
+mac/linux：
+split -b 3000m 大文件
+3000m 是指小文件的大小
+
+小文件合并为大文件
+mac/linux:
+cat 小文件1 小文件2 &gt; 大文件
+
+windows:
+type 小文件1 小文件2 &gt; 大文件
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="计算md5" tabindex="-1"><a class="header-anchor" href="#计算md5" aria-hidden="true">#</a> 计算md5</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>mac：
+md5 文件名
+
+linux:
+md5sum 文件名
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="时间同步" tabindex="-1"><a class="header-anchor" href="#时间同步" aria-hidden="true">#</a> 时间同步</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>ntpdate 时间服务器ip
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,14),m=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),o=e("hr",null,null,-1),b=e("hr",null,null,-1),h=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),n("：你看天上那朵云，像不像你，欠我的100块钱？ "),e("br"),e("br"),e("img",{src:"https://images.pexels.com/photos/12015338/pexels-photo-12015338.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"Picturesque view of vibrant houses stacked on a hill in Riomaggiore, Liguria, Italy.",loading:"lazy"})])],-1);function E(f,g){const i=u("RouterLink");return a(),t("div",null,[c,e("ul",null,[e("li",null,[l(i,{to:"/other/getPath%20vs%20getAbsolutePath%20vs%20getCanonicalPath.html"},{default:s(()=>[n("getPath vs getAbsolutePath vs getCanonicalPath")]),_:1})]),e("li",null,[l(i,{to:"/other/Nacos-Spring%20Gateway-Spring%20boot%E6%97%A0%E6%84%9F%E5%8F%91%E5%B8%83.html"},{default:s(()=>[n("Nacos-Spring Gateway-Spring boot无感发布")]),_:1})]),e("li",null,[l(i,{to:"/other/PostgreSQL%E5%AE%9A%E6%97%B6%E5%A4%87%E4%BB%BD.html"},{default:s(()=>[n("PostgreSQL定时备份")]),_:1})]),e("li",null,[l(i,{to:"/other/Censys%E6%90%9C%E7%B4%A2%E5%BC%95%E6%93%8E%E5%AD%A6%E4%B9%A0.html"},{default:s(()=>[n("Censys搜索引擎学习")]),_:1})]),e("li",null,[l(i,{to:"/software/unclassified/PasteImageIntoMarkdown%E6%8F%92%E4%BB%B6%E5%BC%80%E5%8F%91.html"},{default:s(()=>[n("PasteImageIntoMarkdown插件开发")]),_:1})]),e("li",null,[l(i,{to:"/software/unclassified/%E6%89%B9%E9%87%8F%E4%BF%AE%E6%94%B9git%E5%8E%86%E5%8F%B2%E8%AE%B0%E5%BD%95%E4%B8%AD%E7%9A%84%E7%94%A8%E6%88%B7%E5%90%8D%E5%92%8C%E9%82%AE%E7%AE%B1.html"},{default:s(()=>[n("批量修改git历史记录中的用户名和邮箱")]),_:1})]),e("li",null,[l(i,{to:"/software/unclassified/gperftools.html"},{default:s(()=>[n("gperftools")]),_:1})]),e("li",null,[l(i,{to:"/software/middleware/mysql/%E5%BC%82%E5%9C%B0%E5%A4%9A%E6%B4%BB.html"},{default:s(()=>[n("异地多活")]),_:1})]),e("li",null,[l(i,{to:"/software/middleware/es/ESRally%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95%E6%AD%A5%E9%AA%A4.html"},{default:s(()=>[n("ESRally性能测试步骤")]),_:1})]),e("li",null,[l(i,{to:"/other/GitHub%20Workflow%E7%AA%81%E7%84%B6%E6%8A%A5%E9%94%99.html"},{default:s(()=>[n("GitHub Workflow突然报错")]),_:1})]),e("li",null,[l(i,{to:"/other/Linux%20dev%20shm%E7%9B%AE%E5%BD%95.html"},{default:s(()=>[n("Linux dev shm目录")]),_:1})]),e("li",null,[l(i,{to:"/software/unclassified/%E8%A1%A8%E5%8D%95%E9%87%8D%E5%A4%8D%E6%8F%90%E4%BA%A4%E8%A7%A3%E5%86%B3%E6%96%B9%E6%A1%88.html"},{default:s(()=>[n("表单重复提交解决方案")]),_:1})])]),m,o,b,h])}const x=d(v,[["render",E],["__file","Linux.html.vue"]]);export{x as default};
--- assets/clone-06085faa-2d99ec46.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./graph-e9862b7e-5be23f9b.js";var t=4;function n(r){return o(r,t)}export{n as a};
