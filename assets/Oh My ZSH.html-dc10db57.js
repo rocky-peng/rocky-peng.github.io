00a855ecc46644fcef1c044f41a4af34429ab795
rocky-peng <rocky.peng@qq.com>
2025-04-22 07:19:00 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@e8c2678b9c03076f7c1fa67edf58cbab2d0d1521 🚀

---
+++ assets/Oh My ZSH.html-dc10db57.js
@@ -0,0 +1,46 @@
+import{_ as i,$ as t,a0 as r,a1 as s,a3 as e,a4 as l,a5 as o,a2 as n,w as d}from"./framework-b3a0f150.js";const c={},u=o(`<h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>yum <span class="token function">install</span> <span class="token function">git</span>
+
+yum <span class="token function">install</span> <span class="token function">zsh</span>
+
+<span class="token function">sh</span> <span class="token parameter variable">-c</span> <span class="token string">&quot;<span class="token variable"><span class="token variable">$(</span><span class="token function">curl</span> <span class="token parameter variable">-fsSL</span> https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh<span class="token variable">)</span></span>&quot;</span>
+
+centos7修改主机名
+<span class="token function">vim</span> /etc/hostname        重启
+
+----
+
+centos6修改主机名
+<span class="token function">vim</span> /etc/sysconfig/network  
+添加一行：
+<span class="token assign-left variable"><span class="token environment constant">HOSTNAME</span></span><span class="token operator">=</span>您的主机名
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="更改" tabindex="-1"><a class="header-anchor" href="#更改" aria-hidden="true">#</a> 更改</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> ~/.oh-my-zsh/themes
+
+<span class="token builtin class-name">echo</span> <span class="token variable">$ZSH_THEME</span>          //输出 <span class="token builtin class-name">:</span> robbyrussell
+
+<span class="token function">cp</span> robbyrussell.zsh-theme myrobbyrussell.zsh-theme
+
+<span class="token function">vim</span> myrobbyrussell.zsh-theme
+
+// 修改PROMPT的值为：
+<span class="token assign-left variable">PROMPT</span><span class="token operator">=</span><span class="token string">&#39;%{$fg[green]%}%m@%{$fg[magenta]%}%(?..%?%1v)%n:%{$reset_color%}%{$fg[cyan]%}%~# &#39;</span>
+
+保存退出
+
+修改~/.zshrc文件中ZSH_THEME的值myrobbyrussell
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="安装jdk" tabindex="-1"><a class="header-anchor" href="#安装jdk" aria-hidden="true">#</a> 安装JDK</h1><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>修改 ~/.zshrc文件，在末尾处添加：
+
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">JAVA_8_HOME</span><span class="token operator">=</span>/jdk1.8.0_181
+
+<span class="token builtin class-name">alias</span> <span class="token assign-left variable">jdk8</span><span class="token operator">=</span><span class="token string">&#39;export JAVA_HOME=$JAVA_8_HOME&#39;</span>
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">JAVA_HOME</span><span class="token operator">=</span><span class="token variable">$JAVA_8_HOME</span>
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">CLASSPATH</span><span class="token operator">=</span>.:<span class="token variable">$JAVA_HOME</span>/lib:<span class="token variable">$JAVA_HOME</span>/jre/lib
+
+<span class="token builtin class-name">export</span> <span class="token assign-left variable"><span class="token environment constant">PATH</span></span><span class="token operator">=</span><span class="token variable">$JAVA_HOME</span>/bin:<span class="token environment constant">$PATH</span>
+
+保存退出，执行：
+
+<span class="token builtin class-name">source</span> ~/.zshrc
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,8),p=s("p",null,[s("br"),s("br"),s("br"),s("br"),s("br"),s("br")],-1),v=s("hr",null,null,-1),m=s("hr",null,null,-1),b=s("ul",null,[s("li",null,[s("strong",null,"随机毒鸡汤"),n("：有钱的时候把生活过好，没钱的时候把心情过好，可是没钱心情怎么会好。 "),s("br"),s("br"),s("img",{src:"https://images.pexels.com/photos/19727180/pexels-photo-19727180.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"Serene winter scene of a snowy chalet nestled in a forest, Megève, Auvergne-Rhône-Alpes.",loading:"lazy"})])],-1);function h(E,k){const a=d("RouterLink");return t(),r("div",null,[u,s("ul",null,[s("li",null,[e(a,{to:"/software/middleware/redis/Redis%E7%AC%94%E8%AE%B0.html"},{default:l(()=>[n("Redis笔记")]),_:1})]),s("li",null,[e(a,{to:"/software/middleware/mq/kafka.html"},{default:l(()=>[n("kafka")]),_:1})]),s("li",null,[e(a,{to:"/other/Redis%E9%AB%98%E5%8F%AF%E7%94%A8.html"},{default:l(()=>[n("Redis高可用")]),_:1})]),s("li",null,[e(a,{to:"/software/shodowsocks/ShadowsockServerUpdatePort.html"},{default:l(()=>[n("ShadowsockServerUpdatePort")]),_:1})]),s("li",null,[e(a,{to:"/software/unclassified/%E6%89%B9%E9%87%8F%E4%BF%AE%E6%94%B9git%E5%8E%86%E5%8F%B2%E8%AE%B0%E5%BD%95%E4%B8%AD%E7%9A%84%E7%94%A8%E6%88%B7%E5%90%8D%E5%92%8C%E9%82%AE%E7%AE%B1.html"},{default:l(()=>[n("批量修改git历史记录中的用户名和邮箱")]),_:1})]),s("li",null,[e(a,{to:"/software/middleware/mysql/%E5%BC%82%E5%9C%B0%E5%A4%9A%E6%B4%BB.html"},{default:l(()=>[n("异地多活")]),_:1})]),s("li",null,[e(a,{to:"/software/jvm/JVM%E5%BC%82%E5%B8%B8%E5%A4%84%E7%90%86.html"},{default:l(()=>[n("JVM异常处理")]),_:1})]),s("li",null,[e(a,{to:"/other/PGSQL%E7%9A%84json%E5%92%8C%20jsonb%20%E8%AF%BB%E5%86%99%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95.html"},{default:l(()=>[n("PGSQL的json和 jsonb 读写性能测试")]),_:1})]),s("li",null,[e(a,{to:"/other/Java%E8%AF%AD%E8%A8%80%E5%AE%8C%E6%88%90notion%E7%AC%94%E8%AE%B0%E5%A4%87%E4%BB%BD.html"},{default:l(()=>[n("Java语言完成notion笔记备份")]),_:1})]),s("li",null,[e(a,{to:"/software/middleware/mysql/%E5%88%86%E5%B8%83%E5%BC%8F%E4%BA%8B%E5%8A%A1Seata.html"},{default:l(()=>[n("分布式事务Seata")]),_:1})]),s("li",null,[e(a,{to:"/other/MySQL%E6%95%B0%E6%8D%AE%E8%BF%81%E7%A7%BB%E5%88%B0PGSQL.html"},{default:l(()=>[n("MySQL数据迁移到PGSQL")]),_:1})]),s("li",null,[e(a,{to:"/other/K8S%E5%B8%B8%E7%94%A8%E5%91%BD%E4%BB%A4.html"},{default:l(()=>[n("K8S常用命令")]),_:1})])]),p,v,m,b])}const A=i(c,[["render",h],["__file","Oh My ZSH.html.vue"]]);export{A as default};
---
+++ assets/channel-93f63185-363a1ab9.js
@@ -0,0 +1,1 @@
+import{am as r,an as s}from"./mermaid.esm.min-9a6cab31.js";const n=(a,o)=>r.lang.round(s.parse(a)[o]),e=n;export{e};
