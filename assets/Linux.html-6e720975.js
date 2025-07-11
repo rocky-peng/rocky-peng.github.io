f49dfe75a92845bbee2be62db51ead56c35da120
rocky-peng <rocky.peng@qq.com>
2025-07-11 04:03:33 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@470de3ffd1a011628f4f4745410de1dcadad8cf7 🚀

---
+++ assets/Linux.html-6e720975.js
@@ -0,0 +1,80 @@
+import{_ as d,$ as a,a0 as t,a1 as e,a3 as l,a4 as s,a5 as r,a2 as i,w as u}from"./framework-f719d9e2.js";const v={},c=r(`<h1 id="linux安装字体" tabindex="-1"><a class="header-anchor" href="#linux安装字体" aria-hidden="true">#</a> linux安装字体</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. yum install -y fontconfig mkfontscale
+2. 拷贝字体文件（.ttc文件） 到 /usr/share/fonts/ 下的一个新文件夹(A) 中
+3. chmod -R 755 /usr/share/fonts/A
+4. cd /usr/share/fonts/A
+5. mkfontscale
+6. mkfontdir
+7. fc-cache -fv
+8. 检查是否安装成功：fc-list
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="linux测速" tabindex="-1"><a class="header-anchor" href="#linux测速" aria-hidden="true">#</a> Linux测速</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  
+
+chmod +x speedtest-cli
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="常用命令" tabindex="-1"><a class="header-anchor" href="#常用命令" aria-hidden="true">#</a> 常用命令</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>查看某个目录及子目录占用的空间
+du -h
+ly-zuche-c1@root:/var/log# du -h
+36K	    ./anaconda
+536K	./nginx
+64K	    ./redis
+4.0K	./chrony
+55M	    ./mysql
+4.0K	./ppp
+4.0K	./ntpstats
+5.5M	./httpd
+8.1M	./journal/f9d400c5e1e8c3a8209e990d887d4ac1
+8.1M	./journal
+26M	    ./audit
+181M	./leyong
+12K	    ./tuned
+72M	    ./filebeat
+619M	.
+
+查看某个目录及子目录占用的空间并排序输出
+du -sh * | sort -nr 
+ly-zuche-c1@root:/var# du -sh * | sort -nr 
+619M	log
+370M	cache
+158M	www
+112K	spool
+27G	    lib
+16K	    db  
+12K	    tmp
+12K	    kerberos
+
+查看某个目录占用的空间
+du -sh
+ly-zuche-c1@root:/var# du -sh
+28G	.
+
+查看磁盘使用情况
+df -lh 
+ly-zuche-c1@root:/var# df -lh
+文件系统        容量  已用  可用 已用% 挂载点
+/dev/vda1        50G   34G   14G   72% /
+devtmpfs        1.9G     0  1.9G    0% /dev
+tmpfs           1.9G   24K  1.9G    1% /dev/shm
+tmpfs           1.9G  460K  1.9G    1% /run
+tmpfs           1.9G     0  1.9G    0% /sys/fs/cgroup
+tmpfs           380M     0  380M    0% /run/user/0
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="大分割和小文件合并" tabindex="-1"><a class="header-anchor" href="#大分割和小文件合并" aria-hidden="true">#</a> 大分割和小文件合并</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>大文件分割为小文件
+mac/linux：
+split -b 3000m 大文件
+3000m 是指小文件的大小
+
+小文件合并为大文件
+mac/linux:
+cat 小文件1 小文件2 &gt; 大文件
+
+windows:
+type 小文件1 小文件2 &gt; 大文件
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="计算md5" tabindex="-1"><a class="header-anchor" href="#计算md5" aria-hidden="true">#</a> 计算md5</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>mac：
+md5 文件名
+
+linux:
+md5sum 文件名
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="时间同步" tabindex="-1"><a class="header-anchor" href="#时间同步" aria-hidden="true">#</a> 时间同步</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>ntpdate 时间服务器ip
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,14),m=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),o=e("hr",null,null,-1),b=e("hr",null,null,-1),h=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),i("：今天心情不好删几个微商，让他们知道，创业的道路并不是一帆风顺。 "),e("br"),e("br"),e("img",{src:"https://images.pexels.com/photos/12015338/pexels-photo-12015338.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"Picturesque view of vibrant houses stacked on a hill in Riomaggiore, Liguria, Italy.",loading:"lazy"})])],-1);function f(x,E){const n=u("RouterLink");return a(),t("div",null,[c,e("ul",null,[e("li",null,[l(n,{to:"/software/unclassified/KVM%20%E8%99%9A%E6%8B%9F%E6%9C%BA%E5%AE%89%E8%A3%85.html"},{default:s(()=>[i("KVM 虚拟机安装")]),_:1})]),e("li",null,[l(n,{to:"/software/jvm/JDK%E5%91%BD%E4%BB%A4%E8%A1%8C%E5%B7%A5%E5%85%B7.html"},{default:s(()=>[i("JDK命令行工具")]),_:1})]),e("li",null,[l(n,{to:"/other/K8S%20%E6%97%A0%E6%84%9F%E5%8F%91%E5%B8%83.html"},{default:s(()=>[i("K8S 无感发布")]),_:1})]),e("li",null,[l(n,{to:"/other/Paxos%E7%AE%97%E6%B3%95.html"},{default:s(()=>[i("Paxos算法")]),_:1})]),e("li",null,[l(n,{to:"/software/unclassified/gperftools.html"},{default:s(()=>[i("gperftools")]),_:1})]),e("li",null,[l(n,{to:"/software/unclassified/%E5%88%B6%E4%BD%9CKVM%20ES%E9%95%9C%E5%83%8F%E6%96%87%E4%BB%B6.html"},{default:s(()=>[i("制作KVM ES镜像文件")]),_:1})]),e("li",null,[l(n,{to:"/other/Git%20Merge%20%E3%80%81Rebase.html"},{default:s(()=>[i("Git Merge 、Rebase")]),_:1})]),e("li",null,[l(n,{to:"/other/%E5%89%8D%E7%AB%AFaxios%E4%B8%8B%E8%BD%BDcsv%E6%96%87%E4%BB%B6%E4%B9%B1%E7%A0%81.html"},{default:s(()=>[i("前端axios下载csv文件乱码")]),_:1})]),e("li",null,[l(n,{to:"/software/raft/sofajraft.html"},{default:s(()=>[i("sofajraft")]),_:1})]),e("li",null,[l(n,{to:"/software/unclassified/%E5%88%86%E5%B8%83%E5%BC%8F%E9%97%AE%E9%A2%98.html"},{default:s(()=>[i("分布式问题")]),_:1})]),e("li",null,[l(n,{to:"/other/MySQL%E6%95%B0%E6%8D%AE%E8%BF%81%E7%A7%BB%E5%88%B0PGSQL.html"},{default:s(()=>[i("MySQL数据迁移到PGSQL")]),_:1})]),e("li",null,[l(n,{to:"/other/Linux%20dev%20shm%E7%9B%AE%E5%BD%95.html"},{default:s(()=>[i("Linux dev shm目录")]),_:1})])]),m,o,b,h])}const g=d(v,[["render",f],["__file","Linux.html.vue"]]);export{g as default};
---
+++ assets/NGINX.html-d1305ac8.js
@@ -0,0 +1,36 @@
+import{_ as i,$ as t,a0 as d,a1 as n,a3 as s,a4 as l,a5 as o,a2 as a,w as c}from"./framework-f719d9e2.js";const r={},u=o(`<h1 id="nginx" tabindex="-1"><a class="header-anchor" href="#nginx" aria-hidden="true">#</a> NGINX</h1><h2 id="感性体验下负载均衡配置" tabindex="-1"><a class="header-anchor" href="#感性体验下负载均衡配置" aria-hidden="true">#</a> 感性体验下负载均衡配置</h2><ol><li>本地启动两个jetty</li></ol><p>要达到的效果：通过nginx开启80端口，在浏览通过localhost来访问jetttyA 和 jettyB 提供的服务。</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>jettyA   localhsot:6000    首页输出:6000
+jettyB   localhsot:7000    首页输出:7000
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol start="2"><li>进入nginx配置文件目录</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> /etc/nginx
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><figure><img src="https://cdn.justdopay.top/xiaoshujiang/1629079994607.png" alt="" tabindex="0" loading="lazy"><figcaption>WeChatad950e5ad0c63b5869efc740570cfa47</figcaption></figure><ol start="3"><li>修改 nginx.conf</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>在http节点下新增如下配置：
+
+upstream lyzc <span class="token punctuation">{</span>
+	  ip_hash<span class="token punctuation">;</span>
+	  server localhost:6000<span class="token punctuation">;</span>
+	  server localhost:7000<span class="token punctuation">;</span>
+ <span class="token punctuation">}</span>
+ 
+ 这个配置即为负载均衡的配置，但现在还没有使用。
+ 
+ lyzc ：对集群的命名，可以为域名，名称自己确定
+ ip_hash <span class="token builtin class-name">:</span>  负载均衡算法，可以不写（不写的情况默认为轮询算法）
+ server <span class="token builtin class-name">:</span> 配置提供服务的机器节点信息，可以任意多个
+ 
+ 
+ 然后在server节点下，修改或添加location的配置如下：
+ 
+ location / <span class="token punctuation">{</span>
+		  proxy_pass http://lyzc<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+ 
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="安装nginx" tabindex="-1"><a class="header-anchor" href="#安装nginx" aria-hidden="true">#</a> 安装nginx</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>yum <span class="token function">install</span> nginx
+
+配置文件在  /etc/nginx目录
+
+<span class="token builtin class-name">cd</span> /etc/nginx
+
+<span class="token function">cp</span> nginx.conf  nginx.conf.bak
+
+修改 nginx.conf 文件：参考其他配置
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="https://cdn.justdopay.top/xiaoshujiang/1629080026033.jpg" alt="" loading="lazy"><img src="https://cdn.justdopay.top/xiaoshujiang/1629080008840.jpg" alt="" loading="lazy"></p><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,15),h=n("p",null,[n("br"),n("br"),n("br"),n("br"),n("br"),n("br")],-1),v=n("hr",null,null,-1),E=n("hr",null,null,-1),p=n("ul",null,[n("li",null,[n("strong",null,"随机毒鸡汤"),a("：假如生活出卖了我，我希望是论斤卖。 "),n("br"),n("br"),n("img",{src:"https://images.pexels.com/photos/11035538/pexels-photo-11035538.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"A close-up of a person holding an NGINX sticker with a blurred background.",loading:"lazy"})])],-1);function m(b,g){const e=c("RouterLink");return t(),d("div",null,[u,n("ul",null,[n("li",null,[s(e,{to:"/other/%E5%85%8D%E8%B4%B9https%E8%AF%81%E4%B9%A6%E3%80%81%E8%87%AA%E5%8A%A8%E7%BB%AD%E6%9C%9F.html"},{default:l(()=>[a("免费https证书、自动续期")]),_:1})]),n("li",null,[s(e,{to:"/other/getPath%20vs%20getAbsolutePath%20vs%20getCanonicalPath.html"},{default:l(()=>[a("getPath vs getAbsolutePath vs getCanonicalPath")]),_:1})]),n("li",null,[s(e,{to:"/other/%E5%BE%AE%E5%8D%9A%E5%85%B3%E6%B3%A8%E5%85%B3%E7%B3%BB%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0.html"},{default:l(()=>[a("微博关注关系如何实现")]),_:1})]),n("li",null,[s(e,{to:"/software/unclassified/Markdown%20Mermaid%E7%94%BB%E5%9B%BE%E5%AE%9E%E4%BE%8B.html"},{default:l(()=>[a("Markdown Mermaid画图实例")]),_:1})]),n("li",null,[s(e,{to:"/other/JDK8%20G1%20%E5%A0%86%E5%86%85%E5%AD%98%E5%B1%85%E7%84%B6%E4%B8%8D%E9%87%8A%E6%94%BE.html"},{default:l(()=>[a("JDK8 G1 堆内存居然不释放")]),_:1})]),n("li",null,[s(e,{to:"/other/%E8%A1%8C%E8%BD%AC%E5%88%97%E4%B8%8D%E5%86%8D%E5%A4%8D%E6%9D%82%EF%BC%9ASQL%E9%AB%98%E6%89%8B%E9%83%BD%E5%9C%A8%E7%94%A8%E7%9A%84%E6%8A%80%E5%B7%A7%E6%8F%AD%E7%A7%98.html"},{default:l(()=>[a("行转列不再复杂：SQL高手都在用的技巧揭秘")]),_:1})]),n("li",null,[s(e,{to:"/software/middleware/mysql/MySQL%E7%B4%A2%E5%BC%95.html"},{default:l(()=>[a("MySQL索引")]),_:1})]),n("li",null,[s(e,{to:"/software/unclassified/%E5%A4%9A%E5%8F%B0centos%E6%9C%8D%E5%8A%A1%E5%99%A8%EF%BC%8C%E6%96%87%E4%BB%B6%E4%BA%92%E7%9B%B8%E5%A4%87%E4%BB%BD.html"},{default:l(()=>[a("多台centos服务器，文件互相备份")]),_:1})]),n("li",null,[s(e,{to:"/other/Arthas%E4%BD%BF%E7%94%A8%E8%AE%B0%E5%BD%95.html"},{default:l(()=>[a("Arthas使用记录")]),_:1})]),n("li",null,[s(e,{to:"/other/PGSQL%E7%9A%84json%E5%92%8C%20jsonb%20%E8%AF%BB%E5%86%99%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95.html"},{default:l(()=>[a("PGSQL的json和 jsonb 读写性能测试")]),_:1})]),n("li",null,[s(e,{to:"/software/raft/raft%E5%8D%8F%E8%AE%AE.html"},{default:l(()=>[a("raft协议")]),_:1})]),n("li",null,[s(e,{to:"/software/linux/Linux.html"},{default:l(()=>[a("Linux")]),_:1})])]),h,v,E,p])}const A=i(r,[["render",m],["__file","NGINX.html.vue"]]);export{A as default};
