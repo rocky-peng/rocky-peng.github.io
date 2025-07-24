97055caa589ac7d24195e8161c60c45a569eea84
rocky-peng <rocky.peng@qq.com>
2025-07-24 15:24:18 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@e2a56db35fdf8cfcabef7697035501b28ad88f58 🚀

---
+++ assets/Cordova_Umi项目搭建步骤.html-425fbadf.js
@@ -0,0 +1,81 @@
+import{_ as o,$ as v,a0 as c,a1 as e,a2 as n,a3 as i,a4 as d,a5 as r,w as s}from"./framework-de22589b.js";const t={},u=r(`<h2 id="项目搭建" tabindex="-1"><a class="header-anchor" href="#项目搭建" aria-hidden="true">#</a> 项目搭建</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 创建 cordova 项目： cordova-demo
+cordova create cordova-demo com.example.hello CordovaDemo
+# 上面的命令会自动创建 cordova-demo 目录
+
+
+
+# 开始创建umi项目
+cd cordova-demo
+mkdir myapp-a &amp;&amp; cd myapp-a
+yarn create umi
+
+# 选择app
+# 不启用typescript
+# 勾选antd dva
+# 回车
+
+# 执行完后，执行
+
+yarn or npm install
+
+# 修改 .umirc.js 在 export default { 中添加下面三行代码
+  outputPath: &#39;../www/&#39;,
+  publicPath: &#39;./&#39;,
+  history: &#39;hash&#39;,
+
+# 然后执行
+npm run build
+
+# 上面执行完后，会将 cordova-demo 目录中原有的www目录给删掉，然后重新创建www目录，存放的是umi项目的打包后的文件
+
+# 这个时候返回cordova-demo目录
+cd ..
+
+# 测试 cordova android
+
+cordova platform add android
+cordova build android
+
+# 添加 .gitignore 文件
+
+在 cordova-demo 创建 .gitignore 文件，内容如下：
+
+.idea
+node_modules
+plugins
+platforms
+www
+
+在 myapp-a 创建 .gitignore 文件，内容如下：
+
+
+# dependencies
+/node_modules
+/npm-debug.log*
+/yarn-error.log
+/yarn.lock
+/package-lock.json
+
+# production
+/dist
+
+# misc
+.DS_Store
+
+# umi
+.umi
+.umi-production
+
+结束
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="扩展资料" tabindex="-1"><a class="header-anchor" href="#扩展资料" aria-hidden="true">#</a> 扩展资料</h2>`,3),m={href:"https://cordova.apache.org/docs/en/latest/guide/cli/index.html",target:"_blank",rel:"noopener noreferrer"},b={href:"https://umijs.org/zh/guide/create-umi-app.html#%E5%88%9B%E5%BB%BA-umi-%E9%A1%B9%E7%9B%AE",target:"_blank",rel:"noopener noreferrer"},E=r(`<h2 id="cordova-打包" tabindex="-1"><a class="header-anchor" href="#cordova-打包" aria-hidden="true">#</a> cordova 打包</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>debug包
+
+cordova build android -- --gradleArg=-PcdvVersionCode=2
+
+
+release包
+cordova build android --release -- --gradleArg=-PcdvVersionCode=2
+
+传递参数给gradle
+-- --gradleArg=-PcdvVersionCode=2 --gradleArg=-PVERSION_NAME=2.2.2
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,4),h=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),p=e("hr",null,null,-1),_=e("hr",null,null,-1),B=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),n("：你想努力，但是努力不想你。 "),e("br"),e("br"),e("img",{src:"https://images.pexels.com/photos/33114454/pexels-photo-33114454.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"A woman walks through a sunlit alley in an urban area, shielding her eyes.",loading:"lazy"})])],-1);function g(A,f){const a=s("ExternalLinkIcon"),l=s("RouterLink");return v(),c("div",null,[u,e("p",null,[n("cordova : "),e("a",m,[n("https://cordova.apache.org/docs/en/latest/guide/cli/index.html"),i(a)])]),e("p",null,[n("umi: "),e("a",b,[n("https://umijs.org/zh/guide/create-umi-app.html#创建-umi-项目"),i(a)])]),E,e("ul",null,[e("li",null,[i(l,{to:"/software/java-basic/synchronized%E5%8E%9F%E7%90%86%E6%B7%B1%E5%BA%A6%E5%89%96%E6%9E%90.html"},{default:d(()=>[n("synchronized原理深度剖析")]),_:1})]),e("li",null,[i(l,{to:"/other/CloudFlare%20%E5%AE%A2%E6%88%B7%E7%AB%AF%E8%AF%81%E4%B9%A6%E7%9A%84%E4%BD%BF%E7%94%A8.html"},{default:d(()=>[n("CloudFlare 客户端证书的使用")]),_:1})]),e("li",null,[i(l,{to:"/software/unclassified/Markdown%20Mermaid%E7%94%BB%E5%9B%BE%E5%AE%9E%E4%BE%8B.html"},{default:d(()=>[n("Markdown Mermaid画图实例")]),_:1})]),e("li",null,[i(l,{to:"/software/java-basic/IO%E7%9B%B8%E5%85%B3.html"},{default:d(()=>[n("IO相关")]),_:1})]),e("li",null,[i(l,{to:"/other/Hadoop%20%E4%B8%80.html"},{default:d(()=>[n("Hadoop 一")]),_:1})]),e("li",null,[i(l,{to:"/software/java-basic/%E5%B8%B8%E7%94%A8%E9%98%9F%E5%88%97.html"},{default:d(()=>[n("常用队列")]),_:1})]),e("li",null,[i(l,{to:"/software/unclassified/gperftools.html"},{default:d(()=>[n("gperftools")]),_:1})]),e("li",null,[i(l,{to:"/other/%E5%89%8D%E7%AB%AFaxios%E4%B8%8B%E8%BD%BDcsv%E6%96%87%E4%BB%B6%E4%B9%B1%E7%A0%81.html"},{default:d(()=>[n("前端axios下载csv文件乱码")]),_:1})]),e("li",null,[i(l,{to:"/software/middleware/mysql/%E5%88%86%E5%B8%83%E5%BC%8F%E4%BA%8B%E5%8A%A1Seata.html"},{default:d(()=>[n("分布式事务Seata")]),_:1})]),e("li",null,[i(l,{to:"/software/unclassified/%E6%89%B9%E9%87%8F%E6%9B%BF%E6%8D%A2%E6%96%87%E4%BB%B6%E5%90%8D%E4%B8%AD%E7%9A%84%E6%8C%87%E5%AE%9A%E5%AD%97%E7%AC%A6%E4%B8%B2.html"},{default:d(()=>[n("批量替换文件名中的指定字符串")]),_:1})]),e("li",null,[i(l,{to:"/other/SpringBoot%E6%9C%8D%E5%8A%A1%E5%9C%A8%E6%9C%8D%E5%8A%A1%E5%90%AF%E5%8A%A8%E5%AE%8C%E6%88%90%E5%89%8D%E8%A2%AB%E6%8F%90%E5%89%8D%E6%B3%A8%E5%86%8C%E5%88%B0nacos.html"},{default:d(()=>[n("SpringBoot服务在服务启动完成前被提前注册到nacos")]),_:1})]),e("li",null,[i(l,{to:"/intro.html"},{default:d(()=>[n("intro")]),_:1})])]),h,p,_,B])}const x=o(t,[["render",g],["__file","Cordova_Umi项目搭建步骤.html.vue"]]);export{x as default};
---
+++ assets/Oh My ZSH.html-87dcded7.js
@@ -0,0 +1,46 @@
+import{_ as i,$ as t,a0 as r,a1 as s,a3 as a,a4 as l,a5 as o,a2 as n,w as c}from"./framework-de22589b.js";const d={},u=o(`<h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>yum <span class="token function">install</span> <span class="token function">git</span>
+
+yum <span class="token function">install</span> <span class="token function">zsh</span>
+
+<span class="token function">sh</span> <span class="token parameter variable">-c</span> <span class="token string">&quot;<span class="token variable"><span class="token variable">$(</span><span class="token function">curl</span> <span class="token parameter variable">-fsSL</span> https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh<span class="token variable">)</span></span>&quot;</span>
+
+centos7修改主机名
+<span class="token function">vim</span> /etc/hostname        重启
+
+----
+
+centos6修改主机名
+<span class="token function">vim</span> /etc/sysconfig/network  
+添加一行：
+<span class="token assign-left variable"><span class="token environment constant">HOSTNAME</span></span><span class="token operator">=</span>您的主机名
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="更改" tabindex="-1"><a class="header-anchor" href="#更改" aria-hidden="true">#</a> 更改</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> ~/.oh-my-zsh/themes
+
+<span class="token builtin class-name">echo</span> <span class="token variable">$ZSH_THEME</span>          //输出 <span class="token builtin class-name">:</span> robbyrussell
+
+<span class="token function">cp</span> robbyrussell.zsh-theme myrobbyrussell.zsh-theme
+
+<span class="token function">vim</span> myrobbyrussell.zsh-theme
+
+// 修改PROMPT的值为：
+<span class="token assign-left variable">PROMPT</span><span class="token operator">=</span><span class="token string">&#39;%{$fg[green]%}%m@%{$fg[magenta]%}%(?..%?%1v)%n:%{$reset_color%}%{$fg[cyan]%}%~# &#39;</span>
+
+保存退出
+
+修改~/.zshrc文件中ZSH_THEME的值myrobbyrussell
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="安装jdk" tabindex="-1"><a class="header-anchor" href="#安装jdk" aria-hidden="true">#</a> 安装JDK</h1><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>修改 ~/.zshrc文件，在末尾处添加：
+
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">JAVA_8_HOME</span><span class="token operator">=</span>/jdk1.8.0_181
+
+<span class="token builtin class-name">alias</span> <span class="token assign-left variable">jdk8</span><span class="token operator">=</span><span class="token string">&#39;export JAVA_HOME=$JAVA_8_HOME&#39;</span>
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">JAVA_HOME</span><span class="token operator">=</span><span class="token variable">$JAVA_8_HOME</span>
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">CLASSPATH</span><span class="token operator">=</span>.:<span class="token variable">$JAVA_HOME</span>/lib:<span class="token variable">$JAVA_HOME</span>/jre/lib
+
+<span class="token builtin class-name">export</span> <span class="token assign-left variable"><span class="token environment constant">PATH</span></span><span class="token operator">=</span><span class="token variable">$JAVA_HOME</span>/bin:<span class="token environment constant">$PATH</span>
+
+保存退出，执行：
+
+<span class="token builtin class-name">source</span> ~/.zshrc
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,8),p=s("p",null,[s("br"),s("br"),s("br"),s("br"),s("br"),s("br")],-1),v=s("hr",null,null,-1),b=s("hr",null,null,-1),m=s("ul",null,[s("li",null,[s("strong",null,"随机毒鸡汤"),n("：一直在人生的低谷，大概是因为我恐高。 "),s("br"),s("br"),s("img",{src:"https://images.pexels.com/photos/19727180/pexels-photo-19727180.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"Serene winter scene of a snowy chalet nestled in a forest, Megève, Auvergne-Rhône-Alpes.",loading:"lazy"})])],-1);function h(E,A){const e=c("RouterLink");return t(),r("div",null,[u,s("ul",null,[s("li",null,[a(e,{to:"/other/Notion%E7%AC%94%E8%AE%B0%E5%AE%9A%E6%97%B6%E5%A4%87%E4%BB%BD.html"},{default:l(()=>[n("Notion笔记定时备份")]),_:1})]),s("li",null,[a(e,{to:"/other/%E5%85%8D%E8%B4%B9https%E8%AF%81%E4%B9%A6%E3%80%81%E8%87%AA%E5%8A%A8%E7%BB%AD%E6%9C%9F.html"},{default:l(()=>[n("免费https证书、自动续期")]),_:1})]),s("li",null,[a(e,{to:"/other/MySQL%E9%AB%98%E5%8F%AF%E7%94%A8.html"},{default:l(()=>[n("MySQL高可用")]),_:1})]),s("li",null,[a(e,{to:"/software/java-basic/Thread%20&%20ExecutorService%20&%20ThreadPoolExecutor%20%E6%80%BB%E8%A7%88.html"},{default:l(()=>[n("Thread & ExecutorService & ThreadPoolExecutor 总览")]),_:1})]),s("li",null,[a(e,{to:"/other/%E4%BD%A0%E7%9C%9F%E7%9A%84%E4%BC%9A%E6%8B%BC%E6%8E%A5%E5%AD%97%E7%AC%A6%E4%B8%B2%E5%90%97%EF%BC%9F.html"},{default:l(()=>[n("你真的会拼接字符串吗？")]),_:1})]),s("li",null,[a(e,{to:"/other/%E6%95%B0%E6%8D%AE%E5%90%8C%E6%AD%A5%E6%96%B9%E6%A1%88.html"},{default:l(()=>[n("数据同步方案")]),_:1})]),s("li",null,[a(e,{to:"/software/docker/Centos%E7%A6%BB%E7%BA%BF%E5%AE%89%E8%A3%85Docker.html"},{default:l(()=>[n("Centos离线安装Docker")]),_:1})]),s("li",null,[a(e,{to:"/other/JetBrains-License-Server.html"},{default:l(()=>[n("JetBrains-License-Server")]),_:1})]),s("li",null,[a(e,{to:"/software/unclassified/%E5%89%8D%E5%90%8E%E7%AB%AF%E5%B8%B8%E7%94%A8%E6%8A%80%E6%9C%AF.html"},{default:l(()=>[n("前后端常用技术")]),_:1})]),s("li",null,[a(e,{to:"/software/docker/Docker%E8%B7%A8%E4%B8%BB%E6%9C%BA%E9%80%9A%E4%BF%A1%E6%96%B9%E6%A1%88.html"},{default:l(()=>[n("Docker跨主机通信方案")]),_:1})]),s("li",null,[a(e,{to:"/other/CentOS7%E4%B8%8BDocker%E7%AB%AF%E5%8F%A3%E6%98%A0%E5%B0%84%E5%90%8E%E9%98%B2%E7%81%AB%E5%A2%99%E5%A4%B1%E6%95%88.html"},{default:l(()=>[n("CentOS7下Docker端口映射后防火墙失效")]),_:1})]),s("li",null,[a(e,{to:"/other/PostgreSQL%20JSON%E7%B1%BB%E5%9E%8B%E5%AD%97%E6%AE%B5%E5%B8%B8%E7%94%A8%E6%93%8D%E4%BD%9C.html"},{default:l(()=>[n("PostgreSQL JSON类型字段常用操作")]),_:1})])]),p,v,b,m])}const B=i(d,[["render",h],["__file","Oh My ZSH.html.vue"]]);export{B as default};
