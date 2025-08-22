c87cee4bc1afdcca121c9ce9098fb6a634ee24d7
rocky-peng <rocky.peng@qq.com>
2025-08-22 17:21:01 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@bed1793f2ae34a924876a262a2dbc8b0bcc88087 🚀

---
+++ assets/Docker笔记.html-062a93f2.js
@@ -0,0 +1,35 @@
+import{_ as a,$ as t,a0 as d,a1 as e,a3 as i,a4 as n,a5 as r,a2 as l,w as o}from"./framework-f711b766.js";const c={},u=r(`<p>镜像与容器 === 类与对象</p><h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+yum install -y docker
+
+----
+
+启动
+systemctl start docker.service
+或者
+service docker start
+
+----
+
+设置自启动
+systemctl enable docker.service
+
+----
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="创建自己的镜像" tabindex="-1"><a class="header-anchor" href="#创建自己的镜像" aria-hidden="true">#</a> 创建自己的镜像</h2><p>Dockerfile:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>FROM    sebp/elk
+MAINTAINER rocky &quot;rocky.peng@qq.com&quot;
+RUN apt-get update
+RUN apt-get install htop
+RUN apt-get update &amp;&amp; apt-get install git --assume-yes
+RUN apt-get install zsh --assume-yes
+RUN sh -c &quot;$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)&quot;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在dockerfile所在的目录执行：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>docker build -t pqs/elk . 
+
+-t : 指定镜像id和tag
+. 表示dockerfile所在的目录
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="导入导出镜像" tabindex="-1"><a class="header-anchor" href="#导入导出镜像" aria-hidden="true">#</a> 导入导出镜像</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>导出
+docker save imageid -o 导出的文件名.tar
+
+导入
+docker load -i xxx.tar
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,12),v=e("p",null,[e("br"),e("br"),e("br"),e("br"),e("br"),e("br")],-1),m=e("hr",null,null,-1),E=e("hr",null,null,-1),h=e("ul",null,[e("li",null,[e("strong",null,"随机毒鸡汤"),l("：喝奶茶不一定会胖，因为在发明奶茶之前，就有胖子了。 "),e("br"),e("br"),e("img",{src:"https://images.pexels.com/photos/16520012/pexels-photo-16520012.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"Large cargo vessel docked at Istanbul harbor under dramatic skies, showcasing global trade.",loading:"lazy"})])],-1);function b(p,B){const s=o("RouterLink");return t(),d("div",null,[u,e("ul",null,[e("li",null,[i(s,{to:"/software/unclassified/%E8%AE%B0%E4%B8%80%E6%AC%A1%E5%86%85%E5%AD%98%E6%B3%84%E6%BC%8F.html"},{default:n(()=>[l("记一次内存泄漏")]),_:1})]),e("li",null,[i(s,{to:"/other/JetBrains%20IDE%20%E5%85%A8%E7%A0%B4%E8%A7%A3.html"},{default:n(()=>[l("JetBrains IDE 全破解")]),_:1})]),e("li",null,[i(s,{to:"/software/unclassified/KVM%E6%96%B9%E5%BC%8F%E9%9B%86%E7%BE%A4%E9%83%A8%E7%BD%B2ES.html"},{default:n(()=>[l("KVM方式集群部署ES")]),_:1})]),e("li",null,[i(s,{to:"/other/Hadoop%20%E4%B8%80.html"},{default:n(()=>[l("Hadoop 一")]),_:1})]),e("li",null,[i(s,{to:"/software/unclassified/index%E6%96%B9%E6%B3%95%E5%8E%8B%E5%8A%9B%E6%B5%8B%E8%AF%95%E8%AE%B0%E5%BD%95.html"},{default:n(()=>[l("index方法压力测试记录")]),_:1})]),e("li",null,[i(s,{to:"/other/Paxos%E7%AE%97%E6%B3%95.html"},{default:n(()=>[l("Paxos算法")]),_:1})]),e("li",null,[i(s,{to:"/software/unclassified/gperftools.html"},{default:n(()=>[l("gperftools")]),_:1})]),e("li",null,[i(s,{to:"/other/PGSQL%E7%9A%84json%E5%92%8C%20jsonb%20%E8%AF%BB%E5%86%99%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95.html"},{default:n(()=>[l("PGSQL的json和 jsonb 读写性能测试")]),_:1})]),e("li",null,[i(s,{to:"/other/JetBrains-License-Server.html"},{default:n(()=>[l("JetBrains-License-Server")]),_:1})]),e("li",null,[i(s,{to:"/software/middleware/mysql/SQL%E4%BC%98%E5%8C%96.html"},{default:n(()=>[l("SQL优化")]),_:1})]),e("li",null,[i(s,{to:"/other/Lombok%E7%9A%84Accessors%E5%AF%BC%E8%87%B4EasyExcel%E8%AF%BB%E5%8F%96%E5%A4%B1%E8%B4%A5.html"},{default:n(()=>[l("Lombok的Accessors导致EasyExcel读取失败")]),_:1})]),e("li",null,[i(s,{to:"/other/SpringBoot%E6%9C%8D%E5%8A%A1%E5%9C%A8%E6%9C%8D%E5%8A%A1%E5%90%AF%E5%8A%A8%E5%AE%8C%E6%88%90%E5%89%8D%E8%A2%AB%E6%8F%90%E5%89%8D%E6%B3%A8%E5%86%8C%E5%88%B0nacos.html"},{default:n(()=>[l("SpringBoot服务在服务启动完成前被提前注册到nacos")]),_:1})])]),v,m,E,h])}const A=a(c,[["render",b],["__file","Docker笔记.html.vue"]]);export{A as default};
---
+++ assets/channel-bc508e47-6c4e4639.js
@@ -0,0 +1,1 @@
+import{am as r,an as s}from"./mermaid.esm.min-b59aa34c.js";const n=(a,o)=>r.lang.round(s.parse(a)[o]),e=n;export{e};
