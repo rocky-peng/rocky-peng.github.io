5fc3a813d5767030756d1035d5256f1c879049d9
rocky-peng <rocky.peng@qq.com>
2025-10-03 15:22:53 +0000

    Deploying to gh-pages from @ rocky-peng/rocky-peng.github.io.source@d50a24fd4aa9005f18629d516f56fe887087f608 🚀

---
+++ assets/NGINX.html-e3de32c8.js
@@ -0,0 +1,36 @@
+import{_ as i,$ as t,a0 as d,a1 as n,a3 as a,a4 as l,a5 as c,a2 as e,w as r}from"./framework-4ae679b7.js";const o={},u=c(`<h1 id="nginx" tabindex="-1"><a class="header-anchor" href="#nginx" aria-hidden="true">#</a> NGINX</h1><h2 id="感性体验下负载均衡配置" tabindex="-1"><a class="header-anchor" href="#感性体验下负载均衡配置" aria-hidden="true">#</a> 感性体验下负载均衡配置</h2><ol><li>本地启动两个jetty</li></ol><p>要达到的效果：通过nginx开启80端口，在浏览通过localhost来访问jetttyA 和 jettyB 提供的服务。</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>jettyA   localhsot:6000    首页输出:6000
+jettyB   localhsot:7000    首页输出:7000
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol start="2"><li>进入nginx配置文件目录</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> /etc/nginx
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><figure><img src="https://cdn.justdopay.top/xiaoshujiang/1629079994607.png" alt="" tabindex="0" loading="lazy"><figcaption>WeChatad950e5ad0c63b5869efc740570cfa47</figcaption></figure><ol start="3"><li>修改 nginx.conf</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>在http节点下新增如下配置：
+
+upstream lyzc <span class="token punctuation">{</span>
+	  ip_hash<span class="token punctuation">;</span>
+	  server localhost:6000<span class="token punctuation">;</span>
+	  server localhost:7000<span class="token punctuation">;</span>
+ <span class="token punctuation">}</span>
+ 
+ 这个配置即为负载均衡的配置，但现在还没有使用。
+ 
+ lyzc ：对集群的命名，可以为域名，名称自己确定
+ ip_hash <span class="token builtin class-name">:</span>  负载均衡算法，可以不写（不写的情况默认为轮询算法）
+ server <span class="token builtin class-name">:</span> 配置提供服务的机器节点信息，可以任意多个
+ 
+ 
+ 然后在server节点下，修改或添加location的配置如下：
+ 
+ location / <span class="token punctuation">{</span>
+		  proxy_pass http://lyzc<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+ 
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="安装nginx" tabindex="-1"><a class="header-anchor" href="#安装nginx" aria-hidden="true">#</a> 安装nginx</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>yum <span class="token function">install</span> nginx
+
+配置文件在  /etc/nginx目录
+
+<span class="token builtin class-name">cd</span> /etc/nginx
+
+<span class="token function">cp</span> nginx.conf  nginx.conf.bak
+
+修改 nginx.conf 文件：参考其他配置
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="https://cdn.justdopay.top/xiaoshujiang/1629080026033.jpg" alt="" loading="lazy"><img src="https://cdn.justdopay.top/xiaoshujiang/1629080008840.jpg" alt="" loading="lazy"></p><br><h2 id="系统推荐" tabindex="-1"><a class="header-anchor" href="#系统推荐" aria-hidden="true">#</a> 系统推荐</h2>`,15),v=n("p",null,[n("br"),n("br"),n("br"),n("br"),n("br"),n("br")],-1),h=n("hr",null,null,-1),p=n("hr",null,null,-1),m=n("ul",null,[n("li",null,[n("strong",null,"随机毒鸡汤"),e("：时间不会冲淡痛苦，只会让人习惯痛苦。 "),n("br"),n("br"),n("img",{src:"https://images.pexels.com/photos/11035538/pexels-photo-11035538.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",alt:"A close-up of a person holding an NGINX sticker with a blurred background.",loading:"lazy"})])],-1);function b(E,g){const s=r("RouterLink");return t(),d("div",null,[u,n("ul",null,[n("li",null,[a(s,{to:"/software/unclassified/KVM%20%E8%99%9A%E6%8B%9F%E6%9C%BA%E5%AE%89%E8%A3%85.html"},{default:l(()=>[e("KVM 虚拟机安装")]),_:1})]),n("li",null,[a(s,{to:"/software/middleware/mysql/MySQL%E6%9D%82%E9%A1%B9.html"},{default:l(()=>[e("MySQL杂项")]),_:1})]),n("li",null,[a(s,{to:"/software/spring/Spring%E6%A1%86%E6%9E%B6%E6%BA%90%E7%A0%81%E5%85%B3%E9%94%AE%E7%82%B9.html"},{default:l(()=>[e("Spring框架源码关键点")]),_:1})]),n("li",null,[a(s,{to:"/other/%E6%98%AF%E6%97%B6%E5%80%99%E4%BD%BF%E7%94%A8MapStruct%E6%9B%BF%E4%BB%A3BeanUtils%E4%BA%86.html"},{default:l(()=>[e("是时候使用MapStruct替代BeanUtils了")]),_:1})]),n("li",null,[a(s,{to:"/software/unclassified/%E4%B9%B1%E4%B8%83%E5%85%AB%E7%B3%9F%E7%9A%84%E7%AC%94%E8%AE%B0.html"},{default:l(()=>[e("乱七八糟的笔记")]),_:1})]),n("li",null,[a(s,{to:"/software/java-basic/%E5%B8%B8%E7%94%A8%E9%98%9F%E5%88%97.html"},{default:l(()=>[e("常用队列")]),_:1})]),n("li",null,[a(s,{to:"/software/unclassified/gperftools.html"},{default:l(()=>[e("gperftools")]),_:1})]),n("li",null,[a(s,{to:"/other/PGSQL%E7%9A%84json%E5%92%8C%20jsonb%20%E8%AF%BB%E5%86%99%E6%80%A7%E8%83%BD%E6%B5%8B%E8%AF%95.html"},{default:l(()=>[e("PGSQL的json和 jsonb 读写性能测试")]),_:1})]),n("li",null,[a(s,{to:"/other/JetBrains-License-Server.html"},{default:l(()=>[e("JetBrains-License-Server")]),_:1})]),n("li",null,[a(s,{to:"/other/%E5%89%8D%E7%AB%AFaxios%E4%B8%8B%E8%BD%BDcsv%E6%96%87%E4%BB%B6%E4%B9%B1%E7%A0%81.html"},{default:l(()=>[e("前端axios下载csv文件乱码")]),_:1})]),n("li",null,[a(s,{to:"/other/CentOS7%E4%B8%8BDocker%E7%AB%AF%E5%8F%A3%E6%98%A0%E5%B0%84%E5%90%8E%E9%98%B2%E7%81%AB%E5%A2%99%E5%A4%B1%E6%95%88.html"},{default:l(()=>[e("CentOS7下Docker端口映射后防火墙失效")]),_:1})]),n("li",null,[a(s,{to:"/other/Hbase%20%E6%80%BB%E8%A7%88.html"},{default:l(()=>[e("Hbase 总览")]),_:1})])]),v,h,p,m])}const f=i(o,[["render",b],["__file","NGINX.html.vue"]]);export{f as default};
